c89ff5fccfe779ffcf55e391e42c306efbfa9380
Je Wei Yong <jewei@salescandy.com>
2021-09-29 15:52:45 +0800

    test jay

--- aws-android-sdk-appsync/src/main/java/com/amazonaws/mobileconnectors/appsync/AppSyncOfflineMutationInterceptor.java
+++ aws-android-sdk-appsync/src/main/java/com/amazonaws/mobileconnectors/appsync/AppSyncOfflineMutationInterceptor.java
@@ -368,7 +368,7 @@ class AppSyncOfflineMutationInterceptor implements ApolloInterceptor {
         queueHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Log.v(TAG, "Thread:[" + Thread.currentThread().getId() +"]: processing Mutations");
+                Log.v(TAG, "Thread:[" + Thread.currentThread().getId() +"]: processing Mutations TEST JAY");
                 Message message = new Message();
                 message.obj = new MutationInterceptorMessage();
                 message.what = MessageNumberUtil.SUCCESSFUL_EXEC;
